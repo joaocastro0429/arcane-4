ad7ee0b41bd2580d5b8129a7820ac255db0bdfc3
joaocastro <joaocastro0429@gmail.com>
2026-05-23 21:33:52 -0300

    criando a views do cadastro

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,9 +1,35 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-# Create your views here.3
-
-
+from django.shortcuts import render, redirect
+from django.contrib.messages import constants
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 def cadastro(request):
-    return render(request,'cadastro.html')
+    if request.method == 'GET':
+        return render(request, 'cadastro.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+        
+        if not senha == confirmar_senha:
+            messages.add_message(request, constants.ERROR, 'Senha e confirmar senha não são iguais.')
+            return redirect('cadastro')
+            
+        if len(senha) < 6:
+            messages.add_message(request, constants.ERROR, 'Sua senha deve ter pelo menos 6 caracteres.')
+            return redirect('cadastro')
+
+        users = User.objects.filter(username=username)
+        
+        if users.exists():
+            messages.add_message(request, constants.ERROR, 'Já existe um usuário com esse username.')
+            return redirect('cadastro')
+        
+        User.objects.create_user(
+            username=username,
+            password=senha
+        )
+
+        return redirect('login')
+
+    
